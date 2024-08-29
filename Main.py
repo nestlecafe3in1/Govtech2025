dc019ade781d49dd7866e7fb25fc0148088a159c
Nestlecafe3in1 <jkng007@suss.edu.sg>
2024-08-29 22:04:11 +0800

    started on question 2

--- Main.py
+++ Main.py
@@ -35,7 +35,7 @@ class QuestionOne:
     def __init__(self):
         self.restaurant_url = Q1Constants.restaurant_url
         self.country_code_file = Q1Constants.country_code_file
-        self.restaurant_data = None
+        self.restaurant_df = None
 
     def extract_json(self):
         response = requests.get(self.restaurant_url)
@@ -48,13 +48,13 @@ class QuestionOne:
         json_data = self.extract_json()
         return pd.json_normalize(json_data, column_to_normalize)
 
-    def read_csv(self, csv):
+    def read_csv(self, csv:str):
         try:
             return pd.read_excel(csv)
         except FileNotFoundError:
             print(f"File not found: {csv}")
 
-    def export_df_to_csv(self, df, file_path):
+    def export_df_to_csv(self, df:pd.DataFrame, file_path:str):
         try:
             df.to_csv(file_path, index=False)
             print(f"Restaurant Data exported successfully to {file_path}")
@@ -64,17 +64,41 @@ class QuestionOne:
     def run(self):
         base_restaurant_df = self.create_json_dataframe(Q1Constants.normalize_record_path)
         country_code_df = self.read_csv(self.country_code_file)
-        self.restaurant_data = pd.merge(
+        self.restaurant_df = pd.merge(
             base_restaurant_df, country_code_df, 
             left_on= Q1Constants.left_join_key, right_on=Q1Constants.right_join_key,
             how = "left"
         )
         # Convertion of column datatype to float
-        self.restaurant_data[Q1Constants.column_convert] = self.restaurant_data[Q1Constants.column_convert].astype('float64')
-        df_to_export = (self.restaurant_data.rename(columns=Q1Constants.columns_to_rename)
+        self.restaurant_df[Q1Constants.column_convert] = self.restaurant_df[Q1Constants.column_convert].astype('float64')
+        # Rename and select columns before export
+        df_to_export = (self.restaurant_df.rename(columns=Q1Constants.columns_to_rename)
                                             [Q1Constants.column_selection]
         )
         self.export_df_to_csv(df_to_export, Q1Constants.output_file_path)
 
-class Question2:
+class Q2Constants:
+    event_column_to_unpack = 'restaurant.zomato_events'
+
+class QuestionTwo:    
+    def __init__(self, restaurant_df):
+        self.restaurant_df = restaurant_df
+
+    def column_unpacker(self, df, column_to_unpack):
+        # Expand the lists within the column vertically
+        expanded_df = df.explode(column_to_unpack)
+        # Unpack the dictionaries within the specified column horizontally
+        flattened_df = pd.json_normalize(
+            expanded_df[column_to_unpack]
+        )
+        # Join both dataframes back together
+        merged_df = (expanded_df.drop("column_to_unpack")
+                                .join(flattened_df)
+        )
+        return merged_df
     
+    def month_filter(self)
+
+    def run(self):
+        events_unpacked_df = self.column_unpacker(self.restaurant_df, Q2Constants.event_column_to_unpack)
+        
