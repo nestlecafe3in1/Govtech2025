c736d1505eb8ead503db93f44cdc4f5dba9b7166
nestlecafe3in1 <jkng007@suss.edu.sg>
2024-08-29 20:01:54 +0800

    Updated Main.py

--- Main.py
+++ Main.py
@@ -9,6 +9,23 @@ class Constants:
     q1_json_normalize_column = "restaurants"
     q1_left_on = 'restaurant.location.country_id'
     q1_right_on = 'Country Code'
+    q1_column_dict = {
+        'restaurant.R.res_id': 'Restaurant Id',
+        'restaurant.name': 'Restaurant Name',
+        'restaurant.location.city': 'City',
+        'restaurant.user_rating.votes': 'User Rating Votes',
+        'restaurant.user_rating.aggregate_rating': 'User Aggregate Rating',
+        'restaurant.cuisines': 'Cuisines'
+    }
+    q1_selected_columns = [
+        'Restaurant Id', 
+        'Restaurant Name', 
+        'Country', 
+        'City', 
+        'User Rating Votes', 
+        'User Aggregate Rating', 
+        'Cuisines'
+    ]
 
 
 class QuestionOne:
@@ -24,7 +41,7 @@ class QuestionOne:
         else:
             response.raise_for_status()
 
-    def create_json_dataframe(self, column_to_normalize):
+    def create_json_dataframe(self, column_to_normalize:str):
         json_data = self.extract_json()
         return pd.json_normalize(json_data, column_to_normalize)
 
@@ -43,13 +60,25 @@ class QuestionOne:
 
     def rename_columns(self, df:pd.DataFrame, column_dict:dict):
         return df.rename(columns=column_dict)
+    
+    def select_columns(self, df, selected_columns: list):
+        return df[selected_columns]
+    
+    def change_datatype():
+        
 
     def run(self):
-        base_restaurant_df = self.create_json_dataframe(Constants.json_normalize_column)
+        base_restaurant_df = self.create_json_dataframe(Constants.q1_json_normalize_column)
         country_code_df = self.read_csv()
         self.restaurant_data = self.merge_dataframes(
             base_restaurant_df, country_code_df, 
             left_on= Constants.q1_left_on, 
             right_on=Constants.q1_right_on
         )
+        renamed_df = self.rename_columns(self.restaurant_data, Constants.q1_column_dict)
+        filtered_df = self.select_columns(renamed_df, Constants.q1_selected_columns)
+        print(filtered_df)
 
+
+q1 = QuestionOne()
+q1.run()
